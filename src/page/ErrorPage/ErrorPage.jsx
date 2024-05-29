272c08d9359a1ab72f4695e1aae0cae9080b43c8
ace3366 <poohmandu22@gmail.com>
2024-05-29 10:59:49 +0700

    changing error page

--- src/page/ErrorPage/ErrorPage.jsx
+++ src/page/ErrorPage/ErrorPage.jsx
@@ -9,9 +9,14 @@ export default function ErrorPage() {
         <h2 className="text-6xl font-encode font-semibold text-red-500">
           Oops!
         </h2>
-        <p className="text-2xl mt-10">
-          Look like something went wrong, we're sorry about that
-        </p>
+        <p className="text-2xl mt-10">Look like something went wrong</p>
+        <h3 className="text-2xl mt-10 mb-8">
+          It may take some time to fetch data from the server<br></br> Please
+          stand by or refresh the page
+        </h3>
+        <h3 className="text-xl text-red-500 font-semibold">
+          We're sorry for this inconvenience
+        </h3>
       </div>
     </main>
   );
--- src/page/ProtectRoute.jsx
+++ src/page/ProtectRoute.jsx
@@ -1,27 +1,31 @@
 import React, { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 
-const checkAuth = async () => {
-  try {
-    const response = await fetch(`${process.env.REACT_APP_API}/check-auth`, {
-      method: "GET",
-      credentials: "include",
-    });
-    if (!response.ok) {
-      throw new Error("Can not get auth");
-    }
-    const resData = await response.json();
-
-    return resData.isAuth;
-  } catch (err) {
-    console.log(err);
-  }
-};
-
 const ProtectedRoute = ({ element }) => {
+  const [loaded, setLoaded] = useState(false);
   const [isAuthenticated, setIsAuthenticated] = useState(null);
   // Nếu đã đăng nhập thì sẽ vào route, còn không sẽ bị navigate tới /login
   useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        setLoaded(false);
+        const response = await fetch(
+          `${process.env.REACT_APP_API}/check-auth`,
+          {
+            method: "GET",
+            credentials: "include",
+          }
+        );
+        if (!response.ok) {
+          throw new Error("Can not get auth");
+        }
+        const resData = await response.json();
+        setLoaded(true);
+        return resData.isAuth;
+      } catch (err) {
+        console.log(err);
+      }
+    };
     const verifyAuth = async () => {
       const auth = await checkAuth();
       setIsAuthenticated(auth);
